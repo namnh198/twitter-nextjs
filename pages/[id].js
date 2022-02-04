7710144d21b727e4a9230331dc0e0345ca28a6b7
namnh198 <namptit.dev@gmail.com>
2022-02-05 01:55:39 +0700

    fix not view single post

--- pages/[id].js
+++ pages/[id].js
@@ -1,56 +1,54 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import { getProviders, getSession, useSession } from "next-auth/react";
-import Head from "next/head";
-import Modal from "../components/Modal";
-import Sidebar from "../components/Sidebar";
-import Login from "../components/Login";
-import Post from "../components/Post";
-import Comment from "../components/Comment";
-import { useRouter } from "next/router";
 import {
   collection,
-  onSnapshot,
-  query,
   doc,
+  onSnapshot,
   orderBy,
-} from "firebase/firestore";
-import { db } from "../firebase";
-import { ArrowLeftIcon } from "@heroicons/react/solid";
-import Widgets from "../components/Widgets";
+  query,
+} from "@firebase/firestore";
+import { getProviders, getSession, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import { useRecoilState } from "recoil";
 import { modalState } from "../atoms/modalAtom";
-import { useEffect, useState } from "react";
+import Modal from "../components/Modal";
+import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
+import Post from "../components/Post";
+import Login from "../components/Login";
+import { db } from "../firebase";
+import { ArrowLeftIcon } from "@heroicons/react/solid";
+import Comment from "../components/Comment";
+import Head from "next/head";
 
-export default function PostPage({
-  trendingResults,
-  followResults,
-  providers,
-}) {
+export default function PostPage({ trendingResults, followResults, providers }) {
   const { data: session } = useSession();
-
-  if (!session) return <Login providers={providers} />;
-
-  const isOpen = useRecoilState(modalState);
-  const [post, setPost] = useState(null);
+  const [isOpen, setIsOpen] = useRecoilState(modalState);
+  const [post, setPost] = useState();
   const [comments, setComments] = useState([]);
   const router = useRouter();
   const { id } = router.query;
 
-  useEffect(() => {
-    onSnapshot(doc(db, "posts", id), (snapshot) => {
-      setPost(snapshot.data());
-    });
-  }, [db]);
+  useEffect(
+    () =>
+      onSnapshot(doc(db, "posts", id), (snapshot) => {
+        setPost(snapshot.data());
+      }),
+    [db]
+  );
 
-  useEffect(() => {
-    onSnapshot(
-      query(
-        collection(db, "posts", id, "comments"),
-        orderBy("timestamp", "desc")
+  useEffect(
+    () =>
+      onSnapshot(
+        query(
+          collection(db, "posts", id, "comments"),
+          orderBy("timestamp", "desc")
+        ),
+        (snapshot) => setComments(snapshot.docs)
       ),
-      (snapshot) => setComments(snapshot.docs)
-    );
-  }, [db, id]);
+    [db, id]
+  );
+
+  if (!session) return <Login providers={providers} />;
 
   return (
     <div>
@@ -58,11 +56,9 @@ export default function PostPage({
         <title>
           {post?.username} on Twitter: &quot;{post?.text}&quot;
         </title>
-        <meta name="description" content="Twitter app build by NextJS" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className="min-h-screen flex max-w-[1500px] mx-auto">
+      <main className="bg-black min-h-screen flex max-w-[1500px] mx-auto">
         <Sidebar />
         <div className="flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]">
           <div className="flex items-center px-1.5 py-2 border-b border-gray-700 text-[#d9d9d9] font-semibold text-xl gap-x-4 sticky top-0 z-50 bg-black">
@@ -72,11 +68,10 @@ export default function PostPage({
             >
               <ArrowLeftIcon className="h-5 text-white" />
             </div>
-            <span>Tweet</span>
+            Tweet
           </div>
 
           <Post id={id} post={post} postPage />
-
           {comments.length > 0 && (
             <div className="pb-72">
               {comments.map((comment) => (
